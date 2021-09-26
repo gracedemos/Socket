61b97ba11d171b4d8fb10953a6f6528d95704cd9
graysondemos <graysondemos@gmail.com>
2021-09-25 22:16:54 -0400

    Second commit

--- src/client.cpp
+++ src/client.cpp
@@ -5,18 +5,16 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int conn(const char* addr, int port) {
-    int sock = 0, valread;
+int conn(const char* addr, int port, int* sock) {
     struct sockaddr_in serv_addr;
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     inet_pton(AF_INET, addr, &serv_addr.sin_addr);
-    if(connect(sock, (const sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cout << "[Connection Failed!]\n";
+    if(connect(*sock, (const sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         return -1;
     }
-    return sock;
+    return 0;
 }
 
 void messageHandler(int sock) {
@@ -33,13 +31,14 @@ int main(int argc, const char* argv[]) {
         return -1;
     }
     int sock = 0, valread;
-    int port = 5050;
-    sock = conn(argv[1], port);
-    if(sock < 0) {
+    if(conn(argv[1], std::stoi(argv[2]), &sock) < 0) {
+        std::cout << "[Connection Failed]\n";
         return -1;
     }
+    std::cout << "[Connected to " + (std::string)argv[1] + " on port " + (std::string)argv[2] + "]\n";
     const char* msg = "Yessir!";
     send(sock, msg, strlen(msg), 0);
     std::thread msg_handler(messageHandler, sock);
+    msg_handler.join();
     return 0;
 }
